b5dde1e9b7db50cb72e84525a49e3abf4d5759c7
Abdallah <abdallahchaar90@gmail.comh>
2021-01-06 11:46:29 +0100

    träning om delning

--- main.js
+++ main.js
@@ -26,11 +26,12 @@ var x = "Abdallah",
 
    y = 150,
 
-   z = x - y; //z = "Abdallah" - 150 = Abdallah150
+   z = x - y;  //z = "Abdallah" - 150 = Abdallah150
 
    document.getElementById("test").innerHTML = z;
 */
 
+/*
 var x = "Abdallah",
 
    y = 150,
@@ -38,3 +39,24 @@ var x = "Abdallah",
    z = x + y; //z = "Abdallah" + 150 = Abdallah150
 
    document.getElementById("test").innerHTML = z;
+*/
+
+/*
+var x = 1000,
+
+   y = 100,
+
+   z = x / y; //z = 1000 / 100 = 10
+
+   document.getElementById("test").innerHTML = z;
+   */
+
+
+  var x = 1000,
+
+  y = 200,
+
+  z = x / y; //z = 1000 / 200 = 5
+
+  document.getElementById("test").innerHTML = z;
+  
