bc84ec1d2f649fa0c106d64bb5ea8c9971a9851f
Abdallah <abdallahchaar90@gmail.comh>
2021-01-06 11:37:23 +0100

    Plus och minus

---
+++ main.js
@@ -0,0 +1,21 @@
+/* global console, alert */
+/*  + -  */
+/* */
+/* 
+var x = 100,
+
+    y = 200,
+
+    z = x + y; //z = 100 + 200 = 300
+
+    document.getElementById("test").innerHTML = z;
+    */
+
+   var x = 1000,
+
+   y = 150,
+
+   z = x - y; //z = 1000 - 150 = 850
+
+   document.getElementById("test").innerHTML = z;
+
